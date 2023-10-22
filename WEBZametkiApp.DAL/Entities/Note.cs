2384ae4b2aa132da02d297b11135e23bf88031d5
asmanov <asmanov.a@gmail.com>
2023-10-22 10:52:50 +0300

    Add project files.

---
+++ WEBZametkiApp.DAL/Entities/Note.cs
@@ -0,0 +1,15 @@
+﻿namespace WEBZametkiApp.DAL.Entities
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public Note()
+        {
+            CreatedDate = DateTime.Now;
+        }
+    }
+}
